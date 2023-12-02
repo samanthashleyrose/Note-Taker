fd0ee7e5c0b8b85402a75d9d7cdc42204ffe92d2
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-01 19:50:30 -0500

    Completed readFromFile and writeToFile functions

--- db/db-utils.js
+++ db/db-utils.js
@@ -2,14 +2,17 @@
 const fs = require('fs');
 const path = require('path');
 
+const dbFilePath = path.join(__dirname, 'db.json');
+
 // Function to read from db.json
 const readFromFile = () => {
-
+    const data = fs.readFileSync(dbFilePath, 'utf8');
+    return JSON.parse(data);
 };
   
 // Function to write to db.json
 const writeToFile = (data) => {
-
+    fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2), 'utf8');
 };
   
 module.exports = { readFromFile, writeToFile };
