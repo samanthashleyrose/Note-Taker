838f4191b9308973fd5df3259f89cc8f455889aa
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-01 19:41:17 -0500

    Began structured layout for router.get/post/delete

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,3 +5,24 @@ const path = require('path');
 
 // Read and write functions
 const { readFromFile, writeToFile } = require('../db/db-utils');
+
+// API Routes
+router.get('/notes', (req, res) => {
+    // Read notes from db.json and send as response
+    const notes = readFromFile();
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    // Add a new note to db.json
+    const newNote = req.body;
+    const notes = readFromFile();
+    writeToFile(notes);
+});
+  
+router.delete('/notes/:id', (req, res) => {
+    // Delete a note by ID from db.json
+    const notes = readFromFile();
+});
+
+module.exports = router;
