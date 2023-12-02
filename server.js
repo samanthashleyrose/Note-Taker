a1a39ae3c9591fcb1808a16a3d6495cafb3e9804
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-01 19:34:58 -0500

    Added middleware, static files, routes, and .listen to server.js

--- server.js
+++ server.js
@@ -5,3 +5,18 @@ const apiRoutes = require('./routes/api-routes.js');
 
 const PORT = 3001;
 const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Static files
+app.use(express.static('public'));
+
+// Use routes
+app.use('/', routes);
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
