6d00522f3be53fb56d49c42311a8952df54e7154
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-01 23:01:03 -0500

    Made get routes for html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,2 +1,13 @@
 // GLOBAL VARIABLES & REQUIRES
 const router = require('express').Router();
+
+// HTML Routes
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages/notes.html'))
+});
+
+module.exports = router;
