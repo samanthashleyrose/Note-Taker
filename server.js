937c7237bd84ad5c110b2a4862822ce961fd0c50
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-04 18:07:37 -0500

    Changed port to also have heroku port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const htmlRoutes = require('./routes/html-routes.js');
 const apiRoutes = require('./routes/api-routes.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
